670c31b7e276eb44b7a12eeed21724d39fff4ed1
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-10-01 13:11:46 +0200

    thymeleaf - training

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.cydeo.controller;
 
 
+import com.cydeo.enums.State;
+import com.cydeo.model.User;
 import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -26,13 +30,23 @@ public class UserController {
 
     }
 
-
     @GetMapping("/create-page")
-    public String getFormPage(Model model) {
-        //model.addAttribute("user", new User());
-        //model.addAttribute("states", State.values());
+    public String addUserButton(Model model) {
+        model.addAttribute("user", new User());
+        model.addAttribute("states", State.values());
         return "/user/create-page";
     }
+
+    @PostMapping("/create")
+    public String submitFormButton(@ModelAttribute("user") User user){
+
+        userService.save(user);
+
+        return "redirect:/user/register";
+
+    }
+
+
 
 
 
