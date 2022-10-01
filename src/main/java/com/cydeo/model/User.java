0fdbdc547ca107135e07911f556ebb25e02e9e89
HasanCanAyhan <ayhanhasancan55@gmail.com>
2022-10-01 11:28:25 +0200

    thymeleaf - training

---
+++ src/main/java/com/cydeo/model/User.java
@@ -0,0 +1,18 @@
+package com.cydeo.model;
+
+import com.cydeo.enums.Gender;
+import com.cydeo.enums.State;
+
+public class User {
+
+    private String firstName;
+    private String lastName;
+    private String address;
+    private State state;
+    private int age;
+    private Gender gender;
+    private boolean married;
+
+
+
+}
